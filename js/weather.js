cb07a9b7e4a5d75fab520a031c83d54188ae3291
Gaeso <ghwns2239@gmail.com>
2023-06-09 05:53:05 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,4 @@
-/* function onGeoOk(position) {
+function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     console.log("you live in", lat, lng);
@@ -10,4 +10,4 @@ function onGeoErr() {
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErr);
 
- https://api.openweathermap.org/data/2.5/weather?lat=36.3187703&lon=127.374710&appid=35a5e8e11f38fcc6777041070f20133f */
+ /* https://api.openweathermap.org/data/2.5/weather?lat=36.3187703&lon=127.374710&appid=35a5e8e11f38fcc6777041070f20133f */
