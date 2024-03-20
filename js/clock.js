7eb9fdee7488981a9192de1703d7220bfd1b48af
Gaeso <ghwns2239@gmail.com>
2024-03-21 03:46:44 +0900

    1

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ function sayHello() {
     const hour = String(date.getHours()).padStart(2,"0");
     const min = String(date.getMinutes()).padStart(2,"0");
     const sec = String(date.getSeconds()).padStart(2,"0");
-
+    date.getD
     clock.innerText = `${hour}:${min}:${sec}`;
 }
 
--- js/greetings.js
+++ js/greetings.js
@@ -3,7 +3,7 @@ const loginInput = document.querySelector("#login-form input");
 const greetings = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username"
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
